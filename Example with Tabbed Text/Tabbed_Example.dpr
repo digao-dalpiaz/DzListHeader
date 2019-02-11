8365aa7955dbfff6e9aecf3cf0ad10297b5d3b44
Rodrigo Depiné Dalpiaz <46692097+digao-dalpiaz@users.noreply.github.com>
2019-02-11 11:51:40 -0200

    recreate demos

---
+++ Example with Tabbed Text/Tabbed_Example.dpr
@@ -0,0 +1,14 @@
+program Tabbed_Example;
+
+uses
+  Vcl.Forms,
+  UFrmTabbedExample in 'UFrmTabbedExample.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
